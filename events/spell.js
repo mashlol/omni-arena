2d32248df22cddd6b7575200004db875572c05c9
Kevin Bedi <kevin.m.bedi@gmail.com>
2013-04-26 23:31:01 -0400

    Can't cast if room not full

--- events/spell.js
+++ events/spell.js
@@ -2,14 +2,17 @@ var Spell = require("../models/spell");
 
 module.exports = {
     run: function(connection, collections, data) {
+        if (data.x == null || data.y == null) {
+            return {error: "Invalid spell event."};
+        }
         if (connection.player == null) {
-            return {error: "Invalid spell event"};
+            return {error: "Can't cast if not logged in"};
         }
         if (!connection.player.get('alive')) {
             return {error: "Can't cast while dead."};
         }
-        if (data.x == null || data.y == null) {
-            return {error: "Invalid spell event"};
+        if (collections.players.where({room: connection.room, alive: true}).length < 2) {
+            return {error: "Can't cast spells if room not full."};
         }
         var newID = collections.spells.nextID();
         var newSpell = new Spell({
